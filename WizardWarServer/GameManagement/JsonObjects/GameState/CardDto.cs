2e54bdd63248fc34276d8d5e30eab12321af4af4
Grojman <gomisromanjorge@gmail.com>
2026-08-15 18:29:48 +0200

    m

--- WizardWarServer/GameManagement/JsonObjects/GameState/CardDto.cs
+++ WizardWarServer/GameManagement/JsonObjects/GameState/CardDto.cs
@@ -1,12 +1,22 @@
-public record CardDto(string id, string name, string description, List<string> families, int attack, int health, string type, string imageUrl, bool canPlay, bool hasEffect, int effectTimes)
+public record CardDto(string id, string serverId, string name, string description, List<string> families, int attack, int health, string type, string imageUrl, bool canPlay, bool hasEffect, int effectTimes, int? conditionProgress, int? conditionTarget)
 {
     public static CardDto Generate(CardInstance card, GameState state, bool dontCheck)
     {
-        return new CardDto(card.Id.ToString(), card.Definition.Name, card.Definition.Description, card.CurrentFamilies, card.CurrentAttack, card.CurrentHealth, card.Definition.Type.ToString(), string.IsNullOrWhiteSpace(card.Definition.imageUrl) ? $"{card.Definition.Id}.webp" : card.Definition.imageUrl, dontCheck || (card.CanPlay?.Check(card.Player.Id, card.Player.PlayerTarget!.Id, card, state, null) ?? true), card.SpecialEffects is not null && card.SpecialEffects.Count > 0, card.MaxSpecialEffectTimes);
+        int? conditionProgress = null;
+        int? conditionTarget = null;
+
+        if (card.CanPlay is CountPlayedCardsCondition cpc)
+        {
+            var target = cpc.Target == PlayerType.PLAYER ? card.Player.Id : card.Player.PlayerTarget!.Id;
+            conditionProgress = state.GetState(target).PlayedCards.Count(cpc.Filter.Check);
+            conditionTarget = cpc.Value.Amount;
+        }
+
+        return new CardDto(card.Id.ToString(), card.Definition.Id, card.Definition.Name, card.Definition.Description, card.CurrentFamilies, card.CurrentAttack, card.CurrentHealth, card.Definition.Type.ToString(), string.IsNullOrWhiteSpace(card.Definition.imageUrl) ? $"{card.Definition.Id}.webp" : card.Definition.imageUrl, dontCheck || (card.CanPlay?.Check(card.Player.Id, card.Player.PlayerTarget!.Id, card, state, null) ?? true), card.SpecialEffects is not null && card.SpecialEffects.Count > 0, card.MaxSpecialEffectTimes, conditionProgress, conditionTarget);
     }
 
     public static CardDto Generate(CardDefinition card)
     {
-        return new CardDto(card.Id, card.Name, card.Description, card.Families.ToList(), card.BaseAttack, card.BaseHealth, card.Type.ToString(), string.IsNullOrWhiteSpace(card.imageUrl) ? $"{card.Id}.webp" : card.imageUrl, true, card.PlayEffects is not null && card.PlayEffects.Length > 0, card.PlayEffectTriggerTimes);
+        return new CardDto(card.Id, card.Id, card.Name, card.Description, card.Families.ToList(), card.BaseAttack, card.BaseHealth, card.Type.ToString(), string.IsNullOrWhiteSpace(card.imageUrl) ? $"{card.Id}.webp" : card.imageUrl, true, card.PlayEffects is not null && card.PlayEffects.Length > 0, card.PlayEffectTriggerTimes, null, null);
     }
 }
